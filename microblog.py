2e0d1853addc87518d54cfbf08b001350378409c
Ivan Khokhlov <khokhlov.ivan@gmail.com>
2021-01-16 21:25:05 +0100

    Finished initial DB setup.

--- microblog.py
+++ microblog.py
@@ -1,1 +1,18 @@
-from app import app
+'''
+Core project file which imports the Flask app and sets
+the Flask shell context for ease of use.
+'''
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    '''
+    Add the database, user, and post context to the shell session.
+    When you run 'flask shell' in the command line then the following objects
+    will become available in the session:
+    db
+    Post
+    User
+    '''
+    return {'db': db, 'User': User, 'Post': Post}
