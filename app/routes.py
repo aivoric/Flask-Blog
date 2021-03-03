36d621339c997f3d8e8034deb632c07f1c4bdde8
Ivan Khokhlov <khokhlov.ivan@gmail.com>
2021-03-03 08:58:19 +0300

    Finished password reset with email

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,15 @@
 Module contains all the view functions which handle different routes.
 """
 from datetime import datetime
+from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import render_template, flash, redirect, url_for, request
-from werkzeug.urls import url_parse
 from app import app, db
 from app.models import User, Post
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
+    EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.email import send_password_reset_email
+
 
 @app.before_request
 def before_request():
@@ -104,7 +106,7 @@ def user(username):
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.jnja', user=user, posts=posts.items,
+    return render_template('user.jinja', user=user, posts=posts.items,
                            next_url=next_url, prev_url=prev_url, form=form)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -181,3 +183,18 @@ def reset_password_request():
         return redirect(url_for('login'))
     return render_template('reset_password_request.jinja',
                            title='Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
--- config.py
+++ config.py
@@ -14,11 +14,12 @@ class Config():
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_SERVER = 'smtp.mail.yahoo.com' # os.environ.get('MAIL_SERVER')
+    MAIL_PORT = 587 # int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = True # os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USE_SSL = True # os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['khokhlov.ivan@yahoo.com']
 
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 10
