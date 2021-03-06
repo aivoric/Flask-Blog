afa1858733d7a6f4a76f43a7d21e90297f2581d6
Ivan Khokhlov <khokhlov.ivan@gmail.com>
2021-03-06 11:24:24 +0300

    Added flask_moment (moment.js)

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from config import Config
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
@@ -18,6 +19,7 @@ app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 mail = Mail(app)
+moment = Moment(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
